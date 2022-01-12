c20286d430e413325869e6a3e2e0cc619bab563e
Bagus Andika <bagus.andika@majoo.id>
2022-01-12 14:38:28 +0700

    sign in components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
