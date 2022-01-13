7bd3c02890a71f6302ebb14eb4ae4de45f2905f9
Bagus Andika <bagus.andika@majoo.id>
2022-01-13 14:40:28 +0700

    email password login firebase

--- src/App.js
+++ src/App.js
@@ -35,6 +35,8 @@ class App extends Component {
               id,
               ...snapShot.data(),
             }
+          }, () => {
+            console.log(this.state);
           })
         });
       } else {
